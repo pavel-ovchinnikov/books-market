bb194ca3918c119d24ac49f89060595d7ced0d75
Valia Anashkina <valiaanashkina.life@gmail.com>
2025-03-13 12:29:34 +0700

    fix code

--- src/schema.sql
+++ src/schema.sql
@@ -4,12 +4,11 @@ BEGIN;
 CREATE TABLE IF NOT EXISTS author
 (
 -- This coloumns of the table
-    id uuid,
-    name text,
-    surname text,
-    date_of_birth date,
-    date_of_death date
-	
+	id uuid,
+	name text,
+	surname text,
+	date_of_birth date,
+	date_of_death date
 );
 
 
