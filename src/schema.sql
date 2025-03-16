89d376699659d20bd2280ec77ed8a4deacf5eb74
Valia Anashkina <valiaanashkina.life@gmail.com>
2025-03-16 16:07:59 +0700

    create key

--- src/schema.sql
+++ src/schema.sql
@@ -3,37 +3,42 @@ BEGIN;
 
 CREATE TABLE IF NOT EXISTS author
 (
-	id uuid,
+    id uuid not null,
 	name text,
 	surname text,
 	date_of_birth date,
-	date_of_death date	
+	date_of_death date,
+    PRIMARY KEY (id)	
 );
 
 CREATE TABLE IF NOT EXISTS publishing_house
 (
-    id uuid,
-    name text
+    id uuid not null,
+    name text,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS book_publishing_house
 (
-    id uuid,
+    id uuid not null,
     publishing_house_id uuid,
-    book_id uuid
+    book_id uuid,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS book
 (
-    id uuid,
+    id uuid not null,
     name text,
-    year date
+    year date,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS author_book
 (
-    id uuid,
+    id uuid not null,
     author_book uuid,
-    book_id uuid
+    book_id uuid,
+    PRIMARY KEY (id)
 );
 END;
